8e892a1ba12627c347c82baa0b68744a35b1da5b
uiopqwerty <47082597+uiopqwerty@users.noreply.github.com>
2019-04-14 13:18:13 +0300

    p

--- js/button.js
+++ js/button.js
@@ -1,20 +1,21 @@
 function toggleClass(el, _class) {
     if (el && el.className && el.className.indexOf(_class) >= 0) {
-      var pattern = new RegExp('\\s*' + _class + '\\s*');
-      el.className = el.className.replace(pattern, ' ');
+        var pattern = new RegExp('\\s*' + _class + '\\s*');
+        el.className = el.className.replace(pattern, ' ');
+    } else if (el) {
+        el.className = el.className + ' ' + _class;
+    } else {
+        console.log("Element not found :(")
     }
-    else if (el){
-      el.className = el.className + ' ' + _class;
-    }
-    else {
-      console.log("Element not found :(")
-    }
-  }
-  
-window.onload = function(){
-    const navBtn = document.querySelector("#menu-btn");
+}
 
-    navBtn.addEventListener('click', function(){
+window.onload = function() {
+    const navBtn = document.querySelector("#menu-btn");
+    const menuNav = document.querySelector(".menu-nav")
+    navBtn.addEventListener('click', function() {
         toggleClass(navBtn, 'menu-btn_active')
+    }, false)
+    menuNav.addEventListener('click', function() {
+        toggleClass(menuNav, 'menu-nav_active')
     }, false)
 }
